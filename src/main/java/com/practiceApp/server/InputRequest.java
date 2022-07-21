9d5b70a8fec1cb5f138a11139cee0aaaf1d26241
denisburylov <burylov01@mail.ru>
2022-07-21 16:22:21 +0500

    add parse request

---
+++ src/main/java/com/practiceApp/server/InputRequest.java
@@ -0,0 +1,48 @@
+package com.practiceApp.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class InputRequest {
+    String data;
+    final String request;
+    final String path;
+
+    public String getData() {
+        return data;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getRequest() {
+        return request;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    InputRequest(BufferedReader reader) throws IOException {
+        String[] headerLine = reader.readLine().split(" ");
+        this.request = headerLine[0];
+        this.path = headerLine[1];
+        int lenData = -1;
+        String line;
+        while ((line = reader.readLine()) != null && (line.length() != 0)) {
+            System.out.println(line);
+            if (line.contains("Content-Length:")) {
+                lenData = Integer.parseInt(line.split(" ")[1]);
+            }
+        }
+
+        StringBuilder dataBuilder = new StringBuilder();
+        for (int i = 0; i < lenData; i++) {
+            int sym = reader.read();
+            dataBuilder.append((char) sym);
+        }
+        data = dataBuilder.toString();
+    }
+
+}
--- src/main/java/com/practiceApp/server/ServerConnectionHandler.java
+++ src/main/java/com/practiceApp/server/ServerConnectionHandler.java
@@ -1,52 +1,43 @@
 package com.practiceApp.server;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
-public class ServerConnectionHandler extends Thread{
+public class ServerConnectionHandler extends Thread {
 
-        private final Socket socket;
+    private final Socket socket;
 
-        public ServerConnectionHandler(Socket socket) {
-            this.socket = socket;
-        }
+    public ServerConnectionHandler(Socket socket) {
+        this.socket = socket;
+    }
 
-        @Override
-        public void run() {
-            InputStream inputStream = null;
-            OutputStream outputStream = null;
-            System.out.println(socket);
+    @Override
+    public void run() {
+        OutputStream outputStream = null;
+        System.out.println(socket);
 
-            try {
-                inputStream = socket.getInputStream();
-                outputStream = socket.getOutputStream();
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            InputRequest request = new InputRequest(reader);
+            outputStream = socket.getOutputStream();
 
-            } catch (IOException e) {
-                System.err.println(e.getMessage());
-            } finally {
-                if (inputStream!= null) {
-                    try {
-                        inputStream.close();
-                    } catch (IOException e) {
-                        System.err.println(e.getMessage());
-                    }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    System.err.println(e.getMessage());
                 }
-                if (outputStream!=null) {
-                    try {
-                        outputStream.close();
-                    } catch (IOException e) {
-                        System.err.println(e.getMessage());
-                    }
-                }
-                if (socket!= null) {
-                    try {
-                        socket.close();
-                    } catch (IOException e) {
-                        System.err.println(e.getMessage());
-                    }
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    System.err.println(e.getMessage());
                 }
             }
         }
+    }
 }
