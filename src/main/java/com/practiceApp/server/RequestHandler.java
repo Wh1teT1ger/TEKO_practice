3d9938e19a62ac526e661f060cedf8933606ee12
denisburylov <burylov01@mail.ru>
2022-07-22 21:13:24 +0500

    add JsonSyntaxException and NullPointerException

--- src/main/java/com/practiceApp/server/RequestHandler.java
+++ src/main/java/com/practiceApp/server/RequestHandler.java
@@ -1,6 +1,7 @@
 package com.practiceApp.server;
 
 import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
@@ -25,47 +26,51 @@ public class RequestHandler {
         MongoCollection<Document> products = database.getCollection("products");
         MongoCollection<Document> tempTransactions = database.getCollection("temp_transactions");
 
-        if (checkJsonIsPaymentPossible(json)) {
-            Payment payment = gson.fromJson(json.getAsJsonObject("payment"), Payment.class);
-            String checkProduct = json.get("product").getAsString();
-            Document account = merchantAccount.find(new Document("_id", "merchant_account_id")).first();
-            Document product = products.find(new Document("_id", checkProduct)).first();
-            int amount;
-            if (account.isEmpty()) {
-                Document doc = new Document();
-                doc.append("_id", "merchant_account_id");
-                doc.append("value", 10000000);
-                merchantAccount.insertOne(doc);
-                amount = 10000000;
-            } else {
-                amount = account.getInteger("value");
-            }
-            int amountProducts;
-            if (product.isEmpty()) {
-                Document doc = new Document();
-                doc.append("_id", checkProduct);
-                doc.append("amount", 10);
-                products.insertOne(doc);
-                amountProducts = 10;
-            } else {
-                amountProducts = product.getInteger("amount");
-            }
-            if (amount > payment.getAmount()) {
-                if (amountProducts > 0) {
-                    responseData data1 = isPaymentPossibleData(json);
-                    data = gson.toJson(data1);
+        try {
+            if (checkJsonIsPaymentPossible(json)) {
+                Payment payment = gson.fromJson(json.getAsJsonObject("payment"), Payment.class);
+                String checkProduct = json.get("product").getAsString();
+                Document account = merchantAccount.find(new Document("_id", "merchant_account_id")).first();
+                Document product = products.find(new Document("_id", checkProduct)).first();
+                int amount;
+                if (account.isEmpty()) {
                     Document doc = new Document();
-                    doc.append("_id", data1.getId());
-                    tempTransactions.insertOne(doc);
-
+                    doc.append("_id", "merchant_account_id");
+                    doc.append("value", 10000000);
+                    merchantAccount.insertOne(doc);
+                    amount = 10000000;
                 } else {
-                    data = errorMessage(302, "Item out of stock");
+                    amount = account.getInteger("value");
+                }
+                int amountProducts;
+                if (product.isEmpty()) {
+                    Document doc = new Document();
+                    doc.append("_id", checkProduct);
+                    doc.append("amount", 10);
+                    products.insertOne(doc);
+                    amountProducts = 10;
+                } else {
+                    amountProducts = product.getInteger("amount");
+                }
+                if (amount > payment.getAmount()) {
+                    if (amountProducts > 0) {
+                        responseData data1 = isPaymentPossibleData(json);
+                        data = gson.toJson(data1);
+                        Document doc = new Document();
+                        doc.append("_id", data1.getId());
+                        tempTransactions.insertOne(doc);
+
+                    } else {
+                        data = errorMessage(302, "Item out of stock");
+                    }
+                } else {
+                    data = errorMessage(307, "Not enough money");
                 }
             } else {
-                data = errorMessage(307, "Not enough money");
+                data = errorMessage(402, "Bad json");
             }
-        } else {
-            data = errorMessage(402, "Bad json");
+        } catch (JsonSyntaxException | NullPointerException jse) {
+            data = errorMessage(402, jse.getMessage());
         }
         return data;
     }
@@ -78,41 +83,44 @@ public class RequestHandler {
         MongoCollection<Document> products = database.getCollection("products");
         MongoCollection<Document> tempTransactions = database.getCollection("temp_transactions");
         MongoCollection<Document> transactions = database.getCollection("transactions");
+        try {
+            if (checkJsonResumePayment(json)) {
+                Document account = merchantAccount.find(new Document("_id", "merchant_account_id")).first();
+                int getAccount = account.getInteger("value");
 
-        if (checkJsonResumePayment(json)) {
-            Document account = merchantAccount.find(new Document("_id", "merchant_account_id")).first();
-            int getAccount = account.getInteger("value");
+                String productId = json.get("product").getAsString();
+                Transaction tx = gson.fromJson(json.getAsJsonObject("partner_tx"), Transaction.class);
+                Document doc = tempTransactions.find(new Document("_id", tx.getId())).first();
+                Payment payment = gson.fromJson(json.getAsJsonObject("payment"), Payment.class);
+                Payment scrPayment = gson.fromJson(json.getAsJsonObject("src_payment"), Payment.class);
 
-            String productId = json.get("product").getAsString();
-            Transaction tx = gson.fromJson(json.getAsJsonObject("partner_tx"), Transaction.class);
-            Document doc = tempTransactions.find(new Document("_id", tx.getId())).first();
-            Payment payment = gson.fromJson(json.getAsJsonObject("payment"), Payment.class);
-            Payment scrPayment = gson.fromJson(json.getAsJsonObject("src_payment"), Payment.class);
+                Document product = products.find(new Document("_id", productId)).first();
+                int getAmount = product.getInteger("amount");
 
-            Document product = products.find(new Document("_id", productId)).first();
-            int getAmount = product.getInteger("amount");
+                if (!doc.isEmpty()) {
+                    merchantAccount.updateOne(Filters.eq("_id", "merchant_account_id"),
+                            Updates.set("value", getAccount - payment.getAmount() + scrPayment.getAmount()));
 
-            if (!doc.isEmpty()) {
-                merchantAccount.updateOne(Filters.eq("_id", "merchant_account_id"),
-                        Updates.set("value", getAccount - payment.getAmount() + scrPayment.getAmount()));
+                    products.updateOne(Filters.eq("_id", productId),
+                            Updates.set("amount", getAmount - 1));
 
-                products.updateOne(Filters.eq("_id", productId),
-                        Updates.set("amount", getAmount - 1));
+                    data = PaymentData(tx).toString();
+                    tempTransactions.deleteOne(doc);
 
-                data = PaymentData(tx).toString();
-                tempTransactions.deleteOne(doc);
-
-                Document transaction = new Document();
-                transaction.append("tx", json.getAsJsonObject("partner_tx").toString());
-                transaction.append("product", json.getAsJsonObject("product").toString());
-                transaction.append("payment", json.getAsJsonObject("payment").toString());
-                transaction.append("src_payment", json.getAsJsonObject("src_payment").toString());
-                transactions.insertOne(transaction);
-            } else{
-                data = errorMessage(402, "Transaction didnt start");
+                    Document transaction = new Document();
+                    transaction.append("tx", json.getAsJsonObject("partner_tx").toString());
+                    transaction.append("product", json.getAsJsonObject("product").toString());
+                    transaction.append("payment", json.getAsJsonObject("payment").toString());
+                    transaction.append("src_payment", json.getAsJsonObject("src_payment").toString());
+                    transactions.insertOne(transaction);
+                } else {
+                    data = errorMessage(402, "Transaction didnt start");
+                }
+            } else {
+                data = errorMessage(402, "Bad json");
             }
-        } else {
-            data = errorMessage(402, "Bad json");
+        } catch (JsonSyntaxException | NullPointerException e) {
+            data = errorMessage(402, e.getMessage());
         }
 
         return data;
@@ -124,13 +132,17 @@ public class RequestHandler {
 
         MongoCollection<Document> tempTransactions = database.getCollection("temp_transactions");
 
-        if(checkJsonCancelPayment(json)){
-            Transaction tx = gson.fromJson(json.getAsJsonObject("partner_tx"), Transaction.class);
-            Document doc = tempTransactions.find(new Document("_id", tx.getId())).first();
-            tempTransactions.deleteOne(doc);
-            data = PaymentData(tx).toString();
-        } else{
-            data = errorMessage(402, "Bad json");
+        try {
+            if (checkJsonCancelPayment(json)) {
+                Transaction tx = gson.fromJson(json.getAsJsonObject("partner_tx"), Transaction.class);
+                Document doc = tempTransactions.find(new Document("_id", tx.getId())).first();
+                tempTransactions.deleteOne(doc);
+                data = PaymentData(tx).toString();
+            } else {
+                data = errorMessage(402, "Bad json");
+            }
+        } catch (JsonSyntaxException | NullPointerException e) {
+            data = errorMessage(402, e.getMessage());
         }
 
         return data;
@@ -144,6 +156,7 @@ public class RequestHandler {
         MongoCollection<Document> products = database.getCollection("products");
         MongoCollection<Document> transactions = database.getCollection("transactions");
 
+        try{
         if (checkJsonRollbackPayment(json)) {
             Document account = merchantAccount.find(new Document("_id", "merchant_account_id")).first();
             int getAccount = account.getInteger("value");
@@ -165,11 +178,14 @@ public class RequestHandler {
 
                 data = PaymentData(tx).toString();
                 transactions.deleteOne(doc);
-            } else{
+            } else {
                 data = errorMessage(402, "Transaction transaction is missing");
             }
         } else {
             data = errorMessage(402, "Bad json");
+        }
+        } catch (JsonSyntaxException | NullPointerException e) {
+            data = errorMessage(402, e.getMessage());
         }
 
         return data;
